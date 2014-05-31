a5dba44ef27809da075755b82e0fad6dc49b0453
InstanceLabs <danielratke97@hotmail.de>
2014-05-31 11:55:26 +0200

    Test extended Arena

--- src/com/comze_instancelabs/minigamesapi_example/Main.java
+++ src/com/comze_instancelabs/minigamesapi_example/Main.java
@@ -1,19 +1,52 @@
 package com.comze_instancelabs.minigamesapi_example;
 
+import java.util.ArrayList;
+
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.comze_instancelabs.minigamesapi.Arena;
+import com.comze_instancelabs.minigamesapi.ArenaSetup;
 import com.comze_instancelabs.minigamesapi.MinigamesAPI;
+import com.comze_instancelabs.minigamesapi.PluginInstance;
+import com.comze_instancelabs.minigamesapi.config.ArenasConfig;
+import com.comze_instancelabs.minigamesapi.util.Util;
+import com.comze_instancelabs.minigamesapi.util.Validator;
 
 public class Main extends JavaPlugin {
 
 	MinigamesAPI api = null;
 
 	public void onEnable(){
-		api = MinigamesAPI.getAPI().setupAPI(this);
+		api = MinigamesAPI.getAPI().setupAPI(this, IArena.class);
+		PluginInstance pinstance = MinigamesAPI.getAPI().pinstances.get(this);
+		pinstance.addLoadedArenas(loadArenas(this, pinstance.getArenasConfig()));
+		pinstance.pvp = false;
+		System.out.println(MinigamesAPI.getAPI().pinstances.get(this).getArenas().get(0).toString());
 	}
 
+	public static ArrayList<Arena> loadArenas(JavaPlugin plugin, ArenasConfig cf) {
+		ArrayList<Arena> ret = new ArrayList<Arena>();
+		FileConfiguration config = cf.getConfig();
+		if(!config.isSet("arenas")){
+			return ret;
+		}
+		for (String arena : config.getConfigurationSection("arenas.").getKeys(false)) {
+			if(Validator.isArenaValid(plugin, arena, cf.getConfig())){
+				ret.add(initArena(plugin, arena));
+			}
+		}
+		return ret;
+	}
+	
+	public static IArena initArena(JavaPlugin plugin, String arena){
+		IArena a = new IArena(plugin, arena);
+		a.init(Util.getSignLocationFromArena(plugin, arena), Util.getAllSpawns(plugin, arena), Util.getMainLobby(plugin), Util.getComponentForArena(plugin, arena, "lobby"), ArenaSetup.getPlayerCount(plugin, arena, true), ArenaSetup.getPlayerCount(plugin, arena, false), ArenaSetup.getArenaVIP(plugin, arena));
+		return a;
+	}
+	
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		return api.getCommandHandler().handleArgs(this, "minigamesexample", "/" + cmd.getName(), sender, args);
 	}
